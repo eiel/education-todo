166a5383c4e0040e37d9b69476b3429a99adafc9
Tomohiko Himura <eiel.hal@gmail.com>
2015-04-23 20:53:34 +0900

    少し修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,4 +5,16 @@ class Task < ActiveRecord::Base
     return "低" if priority < 0
     "中"
   end
+
+  def high
+    update(priority: 1);
+  end
+
+  def low
+    update(priority: -1);
+  end
+
+  def middle
+    update(priority: 0);
+  end
 end
